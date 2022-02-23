6b6df29f2cdbfc06a52770be58bf7c1e6dc7c14e
Daniel Barros <daniel.barros@cqumail.com>
2022-02-23 23:06:24 +0800

    feature: add Generate Password functionality.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/* ====================================================
+MENU
+=====================================================*/
 // Selectors
 const genMedNumButton = document.querySelector(".button_genMedNum");
 const genPassButton = document.querySelector(".button_genPass");
@@ -5,30 +8,24 @@ const genTestPatButton = document.querySelector(".button_genTestPat");
 const medAppComponent = document.querySelector(".med-app");
 const passComponent = document.querySelector(".pass-app");
 const testPatComponent = document.querySelector(".test-pat-app");
-const medAppQuantity = document.querySelector(".app__qty-selection-input");
-const medAppGenerateButton = document.querySelector(
-  ".app__qty-selection-generate"
-);
-const medAppDataContainer = document.querySelector(".med-app__data");
 
 // Event Listeners
 genMedNumButton.addEventListener("click", displayGenMedNumApp);
 genPassButton.addEventListener("click", displayPassApp);
 genTestPatButton.addEventListener("click", displayTestPatApp);
-medAppGenerateButton.addEventListener("click", generateMedicareNumbers);
 
 // Event Handlers
 function displayGenMedNumApp() {
+  resetPassApp();
   medAppComponent.style.display = "block";
   genMedNumButton.classList.add("menu__button--active");
-  passComponent.style.display = "none";
   genPassButton.classList.remove("menu__button--active");
   testPatComponent.style.display = "none";
   genTestPatButton.classList.remove("menu__button--active");
 }
 
 function displayPassApp() {
-  medAppComponent.style.display = "none";
+  resetGenMedApp();
   genMedNumButton.classList.remove("menu__button--active");
   passComponent.style.display = "block";
   genPassButton.classList.add("menu__button--active");
@@ -45,12 +42,29 @@ function displayTestPatApp() {
   genTestPatButton.classList.add("menu__button--active");
 }
 
+  
+  
+/* ====================================================
+GENERATE MEDICARE NUMBERS APP
+=====================================================*/
+// Selectors
+const medAppQuantity = document.querySelector(".app__qty-selection-input");
+const medAppGenerateButton = document.querySelector(
+  ".app__qty-selection-generate"
+);
+const medAppDataContainer = document.querySelector(".med-app__data");
+
+// Event Listeners
+medAppGenerateButton.addEventListener("click", generateMedicareNumbers);
+    
+// Event Handlers
 function generateMedicareNumbers() {
   medAppDataContainer.innerHTML = "";
   const quantity = medAppQuantity.value;
   if (quantity < 1 || quantity > 100) {
     window.alert("JS ERROR: The number has to be between 1 and 100. Try again.");
     medAppQuantity.value = "";
+    medAppDataContainer.style.display = "none";
     return;
   }
   medAppDataContainer.style.display = "block";
@@ -58,11 +72,11 @@ function generateMedicareNumbers() {
     const newP = document.createElement("p");
     const node = document.createTextNode(
       index + 1 + ") " + randomMedicareNumber()
-    );
-    newP.appendChild(node);
-    newP.className = "med-app__data-string";
-    medAppDataContainer.appendChild(newP);
-  }
+      );
+      newP.appendChild(node);
+      newP.className = "med-app__data-string";
+      medAppDataContainer.appendChild(newP);
+  }  
 }
 
 // Functions
@@ -77,4 +91,99 @@ function randomMedicareNumber() {
   }
   num.push(sum % 10);
   return num.join("") + "1-1";
+}
+
+function resetGenMedApp() {
+  medAppComponent.style.display = "none";
+  medAppDataContainer.style.display = "none";
+  medAppDataContainer.innerHTML = "";
+  medAppQuantity.value = "";
+}
+
+
+
+/* ====================================================
+GENERATE PASSWORD APP
+=====================================================*/
+//Data
+const smallCapsArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const capsArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+const specialArray = ["~","`","!","@","#","$","%","^","&","*","(",")","-","+","=","[","{","]","}",";",":",",","<",".",">","?"];
+let passwordArray = [];
+
+// Selectors
+const numCharacters = document.querySelector(".pass-app__qty-selection-input");
+const passAppGenerateButton = document.querySelector(
+  ".pass-app__qty-selection-generate");
+const passAppDataContainer = document.querySelector(".pass-app__data");
+const smallCapsCheck = document.querySelector("#smallCaps");
+const capsCheck = document.querySelector("#caps");
+const numbersCheck = document.querySelector("#numbers");
+const specialCheck = document.querySelector("#special");
+
+// Event Listeners
+passAppGenerateButton.addEventListener("click", generatePassword);
+
+// Event Handlers
+function generatePassword() {
+  passAppDataContainer.innerHTML = "";
+  const characters = numCharacters.value;
+  if (characters < 1 || characters > 32) {
+    window.alert("JS ERROR: The number of characters has to be between 1 and 32. Try again.");
+    numCharacters.value = "";
+    passAppDataContainer.style.display = "none";
+    return;
+  }
+  passAppDataContainer.style.display = "block";
+  assemblePasswordArray();
+  let password = "";
+  for (let index = 0; index < characters; index++) {
+    password += passwordArray[getRandomInteger(1,passwordArray.length)];
+  }
+  const newP = document.createElement("p");
+  const node = document.createTextNode(password);
+  newP.appendChild(node);
+  newP.className = "pass-app__data-string";
+  passAppDataContainer.appendChild(newP);
+  passwordArray = [];
+}
+
+// Functions
+function assemblePasswordArray() {
+  if (smallCapsCheck.checked) {
+    smallCapsArray.forEach(element => {
+      passwordArray.push(element);
+    });
+  }
+  if (capsCheck.checked) {
+    capsArray.forEach(element => {
+      passwordArray.push(element);
+    });
+  }
+  if (numbersCheck.checked) {
+    numbersArray.forEach(element => {
+      passwordArray.push(element);
+    });
+  }
+  if (specialCheck.checked) {
+    specialArray.forEach(element => {
+      passwordArray.push(element);
+    });
+  }
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function resetPassApp() {
+  passComponent.style.display = "none";
+  passAppDataContainer.style.display = "none";
+  passAppDataContainer.innerHTML = "";
+  numCharacters.value = "";
+  smallCapsCheck.checked = true;
+  capsCheck.checked = true;
+  numbersCheck.checked = true;
+  specialCheck.checked = true;
 }
